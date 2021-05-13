96cd2325efdca5cd6982cf5aa12d70b7c829c88c
Isuru Rajitha <isuru.rajitha.gamage@gmail.com>
2021-05-13 16:12:01 +0530

    authentication setup

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-})->name('home');
+});
 
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
