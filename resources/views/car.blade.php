6177b47d01593be74533424b862b26599f09152c
Isuru Rajitha <isuru.rajitha.gamage@gmail.com>
2021-05-16 02:43:32 +0530

    Updated pages

--- resources/views/car.blade.php
+++ resources/views/car.blade.php
@@ -6,22 +6,35 @@
 <section class="py-20">
     <div class="max-w-screen-md mx-auto">
         
-        <h4 class="text-xl mb-3 text-gray-200">SUVs</h4>
+        <h4 class="text-xl mb-3 text-gray-200">Toyota</h4>
         <div class="flex flex-wrap mx-2">
             <div class="w-full sm:w-1/2 nb-3 px2">
                 <div class="p-4 bg-gray-500 h-full rounded-lg" >
                     <h3 class="text-xl font-bold mb-3">Land Cruiser 76 GXL</h3>
                     <img src="/images/76GXL.jpg" alt="">
+                    
+                
+                </div>
+        
+            </div>
+
+           <div class="w-full sm:w-1/2 nb-3 px-1">
+                <div class="p-4 bg-gray-500 h-full rounded-lg" >
+                    <h3 class="text-xl font-bold mb-3">Land Cruiser Shara</h3>
+                    <img src="/images/sahara.jpg" alt="">
 
                 </div>
 
             </div>
 
+
         </div>
         
     </div>
 
-    <div class="max-w-screen-md mx-auto">
+    
+    <div class="max-w-screen-md mx-auto py-1">
+        <h4 class="text-xl mb-3 text-gray-200">Nissan</h4>
         <div class="flex flex-wrap mx-2">
             <div class="w-full sm:w-1/2 nb-3 px2">
                 <div class="p-4 bg-gray-500 h-full rounded-lg" >
@@ -37,6 +50,7 @@
     </div>
 
     <div class="max-w-screen-md mx-auto">
+        <h4 class="text-xl mb-3 text-gray-200">Land Rover</h4>
         <div class="flex flex-wrap mx-2">
             <div class="w-full sm:w-1/2 nb-3 px2">
                 <div class="p-4 bg-gray-500 h-full rounded-lg" >
@@ -46,10 +60,26 @@
                 </div>
 
             </div>
-
         </div>
         
     </div>
+    <div class="max-w-screen-md mx-auto">
+        <h4 class="text-xl mb-3 text-gray-200">Isuzu</h4>
+        <div class="flex flex-wrap mx-2">
+            <div class="w-full sm:w-1/2 nb-3 px2">
+                <div class="p-4 bg-gray-500 h-full rounded-lg" >
+                    <h3 class="text-xl font-bold mb-3">Isuzu Trooper</h3>
+                    <img src="/images/Trooper.jpg" alt="">
+
+                </div>
+
+            </div>
+        </div>
+        
+    </div>
+
+
+
 </section>
 
 @endsection
